8294aa5fc6b3e18a29a3c5fc0ad876e36e3b7812
Peter Leung <babyghost@gmail.com>
2016-11-22 10:52:53 +0000

    Initial Commit

---
+++ Git Branch & Merge Conflicts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Branch & Merge Conflicts
+//
+//  Created by Peter Leung on 22/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
