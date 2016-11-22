a878d50b257efec9d5e1354e88932bedb828acd2
Peter Leung <babyghost@gmail.com>
2016-11-22 10:58:47 +0000

    if let added

--- Git Branch & Merge Conflicts/ViewController.swift
+++ Git Branch & Merge Conflicts/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let abc = someClass {
+            print (abc)
+        }
     }
 
     override func didReceiveMemoryWarning() {
